fb68027b2a5adf68f7934b8370112f1ac8b46971
unknown <ghassenyounes89@gmail.com>
2025-08-31 20:18:06 +0100

    Fix image paths for Netlify deployment

--- src/Blog.jsx
+++ src/Blog.jsx
@@ -1,13 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-
+import myImage from './image/Rectangle 57.png'
+import myImage2 from './image/Rectangle 56.png'
+import myImage3 from './image/Rectangle 47.png'
 
 const posts = [
   {
     id: 1,
     title: "New modern sofa is here",
     text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse rhoncus libero, mauris vehicula ut ligula molestie placerat eleifend a mauris. Nullam quis ante, Fusce dui tellus, cursus ut, iaculis ut, volutpat nec, mauris.",
-    image: './image/Rectangle 57.png',
+    image: myImage,
     date: "May 15, 2023",
     author: "Interior design",
     type: "sofa",
@@ -16,7 +18,7 @@ const posts = [
     id: 2,
     title: "New modern sofa is here",
     text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse rhoncus libero, mauris vehicula ut ligula molestie placerat eleifend a mauris. Nullam quis ante, Fusce dui tellus, cursus ut, iaculis ut, volutpat nec, mauris.",
-    image:'./image/Rectangle 56.png' ,
+    image:myImage2 ,
     date: "May 15, 2023",  
     author: "Interior design",
     type: "sofa",
@@ -25,7 +27,7 @@ const posts = [
     id: 3,
     title: "New modern sofa is here",
     text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse rhoncus libero, mauris vehicula ut ligula molestie placerat eleifend a mauris. Nullam quis ante, Fusce dui tellus, cursus ut, iaculis ut, volutpat nec, mauris.",
-    image:'./image/Rectangle 47.png' ,
+    image: myImage3 ,
     date: "May 15, 2023",
     author: "Interior design",
     type: "sofa",
